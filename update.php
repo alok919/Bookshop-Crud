88c791844c897f4082d14964a59902f371db4323
Alok <arjun@gmail.com>
2018-01-31 21:58:43 +0600

    Insert and display part complete

---
+++ update.php
@@ -0,0 +1,31 @@
+<!--<!DOCTYPE html>
+<html>
+<head>
+	<title>Crud app</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+
+	<div class="container">
+
+
+		<input type="hidden" name="id" value="">
+		<form role="form" action="insert.php" method="post">
+			<div class="form-group">
+				<label>Book Title</label>
+				<input type="text" name="btitle" class="form-control" value="">
+			</div>
+
+			<div class="form-group">
+				<label>Book Price</label>
+				<input type="text" name="bprice" class="form-control" value="">
+			</div>
+		<button type="submit" name="submit" class="btn btn-info btn-block">Edit Book</button>		
+		</form>
+	</div>
+
+</body>
+</html>
+-->
